5b0ccff1a7b16b40667b3f06b37162cb1a1ef751
Antoh3 <emmanuelmuuo75@gmail.com>
2025-11-01 12:36:03 +0300

    building the project

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,12 +1,25 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+// import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
 
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
+// export const fontSans = FontSans({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
+
+// export const fontMono = FontMono({
+//   subsets: ["latin"],
+//   variable: "--font-mono",
+// });
+import "@fontsource/inter/variable.css";
+import "@fontsource/fira-code/variable.css";
+
+
+export const fontSans = {
   variable: "--font-sans",
-});
+  className: "font-sans",
+};
 
-export const fontMono = FontMono({
-  subsets: ["latin"],
+export const fontMono = {
   variable: "--font-mono",
-});
+  className: "font-mono",
+};
